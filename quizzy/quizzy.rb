753bf4e26b32be3828cd525f203592ef39383bc4
Martin-Ekeroth-nti-johanneberg <martin.ekeroth@elev.ga.ntig.se>
2021-01-24 23:10:11 +0100

    Klar med Login/register, påbörjar quizzy sidan

--- quizzy/quizzy.rb
+++ quizzy/quizzy.rb
@@ -5,23 +5,54 @@ require 'bcrypt'
 
 enable :sessions
 
-errormsg = ""
+db = SQLite3::Database.new('db/quizzy.db')
+msg = ""
+msgtype = ""
 
 get('/') do
-    slim(:"start", locals:{errormsg:errormsg})
+    slim(:"start", locals:{msg:msg, msgtype:msgtype})
 end
 
 post("/register/new") do
+    errors = []
     username = params[:reg_username]
+    if db.execute("SELECT user_id FROM users WHERE username=?", username)[0]!=nil
+        errors << "There is a user with that name already!"
+    end
     password = params[:reg_password]
     password_confirm = params[:reg_password_confirm]
-    if (password == password_confirm)
-        password_digest = BCrypt::Password.create(password)
-        db = SQLite3::Database.new('db/quizzy.db')
-        db.execute("INSERT INTO users (username, pwdigest, role, ranking) VALUES (?, ?, ?, ?)",username,password_digest, "user", 1000)
+    if (password != password_confirm)
+        errors << "The passwords don't match!"
+    end
+    if errors.length != 0 
+        msgtype="errormsg"
+        msg = "Registration unsuccesful! You got #{errors.length} error(s)! "
+        errors.each { |str| msg += str + " " }
     else
-        errormsg = "Passwords don't match!"
+        password_digest = BCrypt::Password.create(password)
+        db.execute("INSERT INTO users (username, pwdigest, role, ranking) VALUES (?, ?, ?, ?)",username,password_digest, "user", 1000)
+        msg = "Registration succesful!"
+        msgtype = ""
     end
     redirect("/")
-    errormsg=""
+    msg = ""
+    msgtype = ""
+end
+
+post("/login") do
+    username = params[:username]
+    password = params[:password]
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        session[:username] = username
+        redirect("/quizzy")
+    end
+end
+
+get("/quizzy") do
+    slim(:"/quizzy")
 end
