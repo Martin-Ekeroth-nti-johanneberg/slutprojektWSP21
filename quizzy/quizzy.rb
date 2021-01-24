df85c8f4c6a912b52078127a02bb6abd7c58a0d0
Martin-Ekeroth-nti-johanneberg <martin.ekeroth@elev.ga.ntig.se>
2021-01-24 17:13:39 +0100

    Användare kan registrera sig + start på validering

--- quizzy/quizzy.rb
+++ quizzy/quizzy.rb
@@ -5,6 +5,23 @@ require 'bcrypt'
 
 enable :sessions
 
+errormsg = ""
+
 get('/') do
-    slim(:start)
+    slim(:"start", locals:{errormsg:errormsg})
+end
+
+post("/register/new") do
+    username = params[:reg_username]
+    password = params[:reg_password]
+    password_confirm = params[:reg_password_confirm]
+    if (password == password_confirm)
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new('db/quizzy.db')
+        db.execute("INSERT INTO users (username, pwdigest, role, ranking) VALUES (?, ?, ?, ?)",username,password_digest, "user", 1000)
+    else
+        errormsg = "Passwords don't match!"
+    end
+    redirect("/")
+    errormsg=""
 end
